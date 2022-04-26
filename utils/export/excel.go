66b96775a687696a2d9da81ed11b6d3cb8fc06f1
chengkai <ckfavor@163.com>
2022-04-26 10:56:00 +0800

    调整日志导出相关的目录结构

---
+++ utils/export/excel.go
@@ -0,0 +1,35 @@
+package export
+
+import (
+	"errors"
+	"ohurlshortener/core"
+	"strconv"
+
+	"github.com/xuri/excelize/v2"
+)
+
+func AccessLogToExcel(logs []core.AccessLog) ([]byte, error) {
+	if logs == nil {
+		return nil, errors.New("数据为空")
+	}
+	f := excelize.NewFile()
+	index := f.NewSheet("Sheet1")
+	//填充表头
+	f.SetCellValue("Sheet1", "A1", "短链接")
+	f.SetCellValue("Sheet1", "B1", "访问时间")
+	f.SetCellValue("Sheet1", "C1", "访问IP")
+	f.SetCellValue("Sheet1", "D1", "UserAgent")
+	for i := 0; i < len(logs); i++ {
+		f.SetCellValue("Sheet1", "A"+strconv.Itoa(i+2), logs[i].ShortUrl)
+		f.SetCellValue("Sheet1", "B"+strconv.Itoa(i+2), logs[i].AccessTime)
+		f.SetCellValue("Sheet1", "C"+strconv.Itoa(i+2), logs[i].Ip.String)
+		f.SetCellValue("Sheet1", "D"+strconv.Itoa(i+2), logs[i].UserAgent.String)
+	}
+	f.SetActiveSheet(index)
+	if excellBytes, erorrW := f.WriteToBuffer(); erorrW != nil {
+		return nil, erorrW
+	} else {
+		return excellBytes.Bytes(), nil
+	}
+
+}
