40e690d4e54f342332276f88e7e678b30bf9afb1
巴拉迪维 <baratsemet@gmail.com>
2022-09-21 11:22:30 +0800

    bugfix: github issue #11
    1. Github issue https://github.com/barats/ohUrlShortener/issues/11
    2. AdminAuthHandler() try to AbortWithStatus(401) instead of Redirect

--- storage/users_storage_test.go
+++ storage/users_storage_test.go
@@ -22,3 +22,29 @@ func init4Test(t *testing.T) {
 		return
 	}
 }
+
+func TestPasswordBase58Hash(t *testing.T) {
+	type args struct {
+		password string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{name: "TestPasswordBase58Hash", want: "EZ2zQjC3fqbkvtggy9p2YaJiLwx1kKPTJxvqVzowtx6t", wantErr: false, args: args{password: "-2aDzm=0(ln_9^1"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := PasswordBase58Hash(tt.args.password)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("PasswordBase58Hash() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("PasswordBase58Hash() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
